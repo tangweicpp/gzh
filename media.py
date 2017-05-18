757f9e9840b2bff4dc25428d2ee6f7f3436de225
tangwei <454083908@qq.com>
2017-05-18 03:36:57 -0400

    素材

---
+++ media.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+from basic import Basic
+import urllib2
+import poster.encode
+from poster.streaminghttp import register_openers
+
+class Media(object):
+    def __init__(self):
+        register_openers()
+    # upload pictures
+    def upload(self, accessToken, filePath, mediaType):
+        openFile = open(filePath, 'rb')
+        param = {'media':openFile}
+        postData, postHeaders = poster.encode.multipart_encode(param)
+
+        postUrl = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=%s&type=%s" % (accessToken, mediaType)
+        request = urllib2.Request(postUrl, postData, postHeaders)
+        urlResp = urllib2.urlopen(request)
+        print urlResp.read()
+
+if __name__ == '__main__':
+    myMedia = Media()
+    accessToken = Basic().get_access_token()
+    filePath = ''
+    mediaType = 'image'
+    myMedia.upload(accessToken, filePath, mediaType)
